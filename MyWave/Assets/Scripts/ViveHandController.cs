17bc8d87dc7b33081b4d31ba048a00ec687f9d26
Huangyujie1 <281254866@qq.com>
2021-04-12 17:27:27 +0800

    MyWave

---
+++ MyWave/Assets/Scripts/ViveHandController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+using System;
+
+/// <summary>
+/// vive手柄控制器
+/// </summary>
+public class ViveHandController : MonoBehaviour
+{
+    
+    [SerializeField]
+    private HandRole handRole;
+    /// <summary>
+    /// 设置左右手
+    /// </summary>
+    private void SetHandRole()
+    {
+        ViveRoleSetter setter = GetComponent<ViveRoleSetter>();
+        handRole = setter.viveRole.ToRole<HandRole>();
+    }
+    /// <summary>
+    /// app退出
+    /// </summary>
+    private void AppQuit()
+    {
+        int index = ViveInput.ClickCount(handRole, ControllerButton.Menu);
+        if (index >= 2)
+        {
+            Application.Quit();
+        }
+    }
+
+    #region MonoBehaviour
+    void Awake()
+    {
+        SetHandRole();
+        //双击菜单键退出app
+        ViveInput.AddClick(handRole, ControllerButton.Menu, AppQuit);
+        //ViveInput.clickInterval = 1;
+
+    }
+    void Start()
+    {
+       
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    #endregion
+}
